2beb622280177d702d0365bc0d2b4e9ca229f254
Jason <52143580+Swishilicous@users.noreply.github.com>
2020-06-07 21:00:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Plugin } = require("powercord/entities");
 const { inject, uninject } = require("powercord/injector");
 const { getModule } = require("powercord/webpack");
 
-module.exports = class grammarNazi extends Plugin {
+module.exports = class GrammarNazi extends Plugin {
 	constructor() {
 		super();
 	}
@@ -15,18 +15,35 @@ module.exports = class grammarNazi extends Plugin {
 		const MessageEvents = await getModule(["sendMessage", "editMessage"]);
 		inject("send.", MessageEvents, "sendMessage", function (args) {
 			let text = args[1].content.trim();
+			var question = false;
+			const questionWords = ['who', 'what', 'when', 'where', 'why', 'how', 'can i'];
+            // Detect if message is a question
+			for(let k = 0; k < questionWords.length; k++){
+				question = (text.includes(questionWords[k])) ? true : false;
+				if(question){
+				  break;
+				}
+			  }
 			// Detect if message is a link
-			if (!(text.slice(0,8) == "https://") && !(text.slice(0,7) == "http://")) {
-				// Forces Capital Letters
-				text = text.charAt(0).toUpperCase() + text.slice(1);
-				// Forces Punctuation
-				text = (text[text.length - 1] == "!" || text[text.length - 1] == "?" || text[text.length - 1] == ".") ?  text : text + '.';
-				// Forces Upercase "I"s
+			if (text.slice(0,8) == "https://" || text.slice(0,7) == "http://") {
+			} else if(question == true) {
+				text = text.charAt(0).toUpperCase() + text.slice(1) + '?';
 				text = text.replace(/i /g, "I ");
-			}
-			args[1].content = text;
+				if (text.slice(text.length-2) == "i?") {
+					text = text.slice(0,text.length-2) + "I?";
+				} 
+            } else {
+                // Forces Capital Letters
+                text = text.charAt(0).toUpperCase() + text.slice(1);
+                // Forces Punctuation
+                text = (text[text.length - 1] == "!" || text[text.length - 1] == "?" || text[text.length - 1] == ".") ?  text : text + '.';
+                // Forces Upercase "I"s
+				text = text.replace(/i /g, "I ");
+            }
 
-			return args;
+			args[1].content = text;
+            return args;
+
 		}, true);
 	}
 
