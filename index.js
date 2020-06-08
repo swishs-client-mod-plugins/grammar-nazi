ffe59e2c223e7893acd5e6775ab7b49eaddd34c7
Jason <52143580+Swishilicous@users.noreply.github.com>
2020-06-07 19:14:25 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Plugin } = require("powercord/entities");
 const { inject, uninject } = require("powercord/injector");
 const { getModule } = require("powercord/webpack");
 
-module.exports = class Period extends Plugin {
+module.exports = class grammarNazi extends Plugin {
 	constructor() {
 		super();
 	}
@@ -15,13 +15,15 @@ module.exports = class Period extends Plugin {
 		const MessageEvents = await getModule(["sendMessage", "editMessage"]);
 		inject("send.", MessageEvents, "sendMessage", function (args) {
 			let text = args[1].content.trim();
-			// Forces Capital Letters
-			text = text.charAt(0).toUpperCase() + text.slice(1);
-			// Forces Punctuation
-			text = (text[text.length - 1] == "!" || text[text.length - 1] == "?" || text[text.length - 1] == ".") ?  text : text + '.';
-			// Forces Upercase "I"s
-			text = text.replace(/i /g, "I ");
-
+			// Detect if message is a link
+			if (!(text.slice(0,8) == "https://") && !(text.slice(0,7) == "http://")) {
+				// Forces Capital Letters
+				text = text.charAt(0).toUpperCase() + text.slice(1);
+				// Forces Punctuation
+				text = (text[text.length - 1] == "!" || text[text.length - 1] == "?" || text[text.length - 1] == ".") ?  text : text + '.';
+				// Forces Upercase "I"s
+				text = text.replace(/i /g, "I ");
+			}
 			args[1].content = text;
 
 			return args;
