464a71ab4c6feeca697be8904a7b2ab726e1c75f
Jason <52143580+Swishilicous@users.noreply.github.com>
2020-06-08 18:13:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,26 +12,37 @@ module.exports = class GrammarNazi extends Plugin {
 	}
 
 	async inject() {
-		const MessageEvents = await getModule(["sendMessage", "editMessage"]);
+		const MessageEvents = await getModule(["sendMessage"]);
 		inject("send.", MessageEvents, "sendMessage", function (args) {
 			let text = args[1].content.trim();
 			var question = false;
+			var apoth = false;
+			var textBeg = text.slice(0,6);
 			const questionWords = ['who', 'what', 'when', 'where', 'why', 'how', 'can i'];
+			const apothWords = ['doesnt', 'cant', 'wont', 'dont', 'ive', 'id', 'im', 'shes', 'hes', 'its', 'theres', 'theyre', 'youve', 'youre', 'couldnt', 'shouldnt', 'wouldnt', 'lets'];
             // Detect if message is a question
-			for(let k = 0; k < questionWords.length; k++){
-				question = (text.includes(questionWords[k])) ? true : false;
+			for(let k = 0; k < questionWords.length; k++){				
+				question = (textBeg.includes(questionWords[k])) ? true : false;
 				if(question){
 				  break;
 				}
-			  }			  
-			// Detect if message is a link
-			if (text.slice(0,8) == "https://" || text.slice(0,7) == "http://") {
-			} else if(text.slice(0,3) == "```") {
+			  }
+			// Detect if message has apothWords	  
+			for(let k = 0; k < apothWords.length; k++){
+				apoth = (text.includes(apothWords[k])) ? true : false;
+				text = text.replace(/doesnt /g, "doesn't ").replace(/cant /g, "can't ").replace(/wont /g, "won't ").replace(/dont /g, "don't ").replace(/ive /g, "I've ").replace(/id /g, "I'd ").replace(/im /g, "I'm ").replace(/shes /g, "she's ").replace(/hes /g, "he's ").replace(/its /g, "it's ").replace(/theres /g, "there's ").replace(/theyre /g, "they're ").replace(/youve /g, "you've ").replace(/youre /g, "you're ").replace(/couldnt /g, "couldn't ").replace(/shouldnt /g, "shouldn't ").replace(/wouldnt /g, "wouldn't ").replace(/lets /g, "let's "); //lmao wtf is this line t-t
+				if(apoth){
+				  break;
+				}
+			  }
+
+			if (text.slice(0,8) == "https://" || text.slice(0,7) == "http://") { // Message Link Detection
+			} else if(text.slice(0,3) == "```") { // Code Block Detection
 			} else if(question == true) {
 				text = text.charAt(0).toUpperCase() + text.slice(1) + '?';
 				text = text.replace(/i /g, "I ");
-				if (text.slice(text.length-2) == "i?") {
-					text = text.slice(0,text.length-2) + "I?";
+				if (text.slice(text.length-2) == "i?") { 
+					text = text.slice(0,text.length-2) + "I?"; // Correct sentences like "Who am I?"
 				} 
             } else {
                 // Forces Capital Letters
