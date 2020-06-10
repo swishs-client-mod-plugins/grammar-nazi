dfb95af8c66a9e6a2147a0595cd8174bf8a5a1e3
Jason <52143580+Swishilicous@users.noreply.github.com>
2020-06-10 16:20:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,95 +12,87 @@ module.exports = class GrammarNazi extends Plugin {
 			label: 'Grammar Nazi',
 			render: Settings
 		})
-		/*powercord.api.settings.registerSettings('capitalize', {
-            category: this.entityID,
-			label: 'Grammar Nazi',
-			render: Settings
-		})*/
 
-/*module.exports = class GrammarNazi extends Plugin {
-	constructor() {
-		super();
-	}
-
-	async startPlugin() {
-		this.inject();
-	}*/
-	if (this.settings.get('punctuation')) {
+	var punct = this.settings.get('punctuation');
+	var capt = this.settings.get('capitalize');
+	var properis = this.settings.get('proper is');
+	var quWords = this.settings.get('questionwords');
+	var apWords = this.settings.get('apothwords');
 		const MessageEvents = await getModule(["sendMessage"]);
 		inject("send.", MessageEvents, "sendMessage", function (args) {
 			let text = args[1].content.trim();
-			text = (text[text.length - 1] == "!" || text[text.length - 1] == "?" || text[text.length - 1] == ".") ?  text : text + '.';
-
-			args[1].content = text;
-            return args;
-		}, true);
-	}
-	if (this.settings.get('ignorebot')) {
-		const MessageEvents = await getModule(["sendMessage"]);
-		inject("send.", MessageEvents, "sendMessage", function(args) {
-			let message = args[1].content.trim();
+			var pretext = text;
+			var botcmd = false;
 			const botPrefix = ['!', '.', 'pls', ';;', ';', '?'];
-			for (let k = 0; k < botPrefix.length; k++) {
-				if (text.startsWith(botPrefix[k])) ? true : false) {
-					return;
+			
+			//Question Words
+			if (quWords) {
+				const questionWords = ['who', 'what', 'when', 'where', 'why', 'how', 'can i'];
+				var question = false;
+				var textBeg = text.slice(0,6);
+				for(let k = 0; k < questionWords.length; k++){				
+					question = (textBeg.includes(questionWords[k])) ? true : false;
+					if(question){
+				 	 break;
+					}
+			  	}
+			}
+
+			// Apostrophe Words
+			if (apWords) {
+				var apoth = false;
+				const apothWords = ['doesnt', 'cant', 'wont', 'dont', 'ive', 'id', 'im', 'shes', 'hes', 'its', 'theres', 'theyre', 'youve', 'youre', 'couldnt', 'shouldnt', 'wouldnt', 'lets']; 
+				for(let k = 0; k < apothWords.length; k++){
+					apoth = (text.includes(apothWords[k])) ? true : false;
+					text = text.replace(/ doesnt /g, " doesn't ").replace(/ cant /g, " can't ").replace(/ wont /g, " won't ").replace(/ dont /g, " don't ").replace(/ ive /g, " I've ").replace(/ id /g, " I'd ").replace(/ im /g, " I'm ").replace(/ shes /g, " she's ").replace(/ hes /g, " he's ").replace(/ its /g, " it's ").replace(/ theres /g, " there's ").replace(/ theyre /g, " they're ").replace(/ youve /g, " you've ").replace(/ youre /g, " you're ").replace(/ couldnt /g, " couldn't ").replace(/ shouldnt /g, " shouldn't ").replace(/ wouldnt /g, " wouldn't ").replace(/ lets /g, " let's "); //lmao wtf is this line t-t
+					if(apoth){
+				  	return;
+					}
 				}
 			}
-		});
-	}
-	/*async inject() {
-		const MessageEvents = await getModule(["sendMessage"]);
-		inject("send.", MessageEvents, "sendMessage", function (args) {
-			let text = args[1].content.trim();
-			var question = false;
-			var apoth = false;
-			var textBeg = text.slice(0,6);
-			const questionWords = ['who', 'what', 'when', 'where', 'why', 'how', 'can i'];
-			const apothWords = ['doesnt', 'cant', 'wont', 'dont', 'ive', 'id', 'im', 'shes', 'hes', 'its', 'theres', 'theyre', 'youve', 'youre', 'couldnt', 'shouldnt', 'wouldnt', 'lets'];
-            // Detect if message is a question
-			for(let k = 0; k < questionWords.length; k++){				
-				question = (textBeg.includes(questionWords[k])) ? true : false;
-				if(question){
-				  break;
-				}
-			  }
-			// Detect if message has apothWords	  
-			for(let k = 0; k < apothWords.length; k++){
-				apoth = (text.includes(apothWords[k])) ? true : false;
-				text = text.replace(/ doesnt /g, " doesn't ").replace(/ cant /g, " can't ").replace(/ wont /g, " won't ").replace(/ dont /g, " don't ").replace(/ ive /g, " I've ").replace(/ id /g, " I'd ").replace(/ im /g, " I'm ").replace(/ shes /g, " she's ").replace(/ hes /g, " he's ").replace(/ its /g, " it's ").replace(/ theres /g, " there's ").replace(/ theyre /g, " they're ").replace(/ youve /g, " you've ").replace(/ youre /g, " you're ").replace(/ couldnt /g, " couldn't ").replace(/ shouldnt /g, " shouldn't ").replace(/ wouldnt /g, " wouldn't ").replace(/ lets /g, " let's "); //lmao wtf is this line t-t
-				if(apoth){
-				  break;
-				}
-			  }
-
-			if (text.slice(0,8) == "https://" || text.slice(0,7) == "http://") { // Message Link Detection
-			} else if(text.slice(0,3) == "```") { // Code Block Detection
-			} else if(question == true) {
+			
+			if(question) {
 				text = text.charAt(0).toUpperCase() + text.slice(1) + '?';
 				text = text.replace(/ i /g, " I ");
 				if (text.slice(text.length-2) == "i?") { 
 					text = text.slice(0,text.length-2) + "I?"; // Correct sentences like "Who am I?"
 				} 
-            } else {
-                // Forces Capital Letters
-                text = text.charAt(0).toUpperCase() + text.slice(1);
-                // Forces Punctuation
-                text = (text[text.length - 1] == "!" || text[text.length - 1] == "?" || text[text.length - 1] == ".") ?  text : text + '.';
-                // Forces Upercase "I"s
+			}
+
+			for (let k = 0; k < botPrefix.length; k++) {
+				botcmd = (text.startsWith(botPrefix[k])) ? true : false;
+				if (botcmd) {
+					return;
+				}
+			}
+
+
+			if (punct) {
+				text = (text[text.length - 1] == "!" || text[text.length - 1] == "?" || text[text.length - 1] == ".") ?  text : text + '.';
+			}
+
+			if (capt) {
+				text = text.charAt(0).toUpperCase() + text.slice(1);
+			}
+			if (properis) {
 				text = text.replace(/ i /g, " I ");
-            }
-
-			args[1].content = text;
+			}
+			
+			if (text.toLowerCase().slice(0,8) == "https://" || text.slice(0,7) == "http://") { // Message Link Detection
+				text = pretext;
+			} else if(text.slice(0,3) == "```") { // Code Block Detection
+				text = pretext;
+			} else if(botcmd) {
+				text = pretext;
+			} else {
+			    args[1].content = text;
+			}
             return args;
-
 		}, true);
-	}*/
 }
 
 	pluginWillUnload() {
 		powercord.api.settings.unregisterSettings('Grammar Nazi')
-		//powercord.api.settings.unregisterSettings('capitalize')
-        //uninject('punctuation')
 		uninject("send.");
 	}
 };
